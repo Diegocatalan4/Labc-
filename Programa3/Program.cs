d8357c9d1df780ce67d9f5b91b937855119d7931
Diegocatalan4 <161140994+Diegocatalan4@users.noreply.github.com>
2024-10-26 21:29:03 -0600

    Add files via upload

---
+++ Programa3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class CantidadDigitos
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un número entero positivo de hasta tres dígitos: ");
+        
+        if (int.TryParse(Console.ReadLine(), out int numero) && numero > 0)
+        {
+            if (numero < 10)
+            {
+                Console.WriteLine("El número tiene 1 dígito.");
+            }
+            else if (numero < 100)
+            {
+                Console.WriteLine("El número tiene 2 dígitos.");
+            }
+            else if (numero < 1000)
+            {
+                Console.WriteLine("El número tiene 3 dígitos.");
+            }
+            else
+            {
+                Console.WriteLine("Error: El número tiene más de 3 dígitos.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Ingrese un número entero positivo de hasta tres dígitos.");
+        }
+    }
+}
